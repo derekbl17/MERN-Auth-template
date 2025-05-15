f54360af5b97c2b8eecc159fa410a8bebc605d55
derekbl17 <derekc1fill@gmail.com>
2025-05-15 17:58:16 +0300

    reduxed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import store from "./store.js";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,12 +20,19 @@ const router = createBrowserRouter([
       { index: true, element: <HomeScreen /> },
       { path: "login", element: <LoginScreen /> },
       { path: "register", element: <RegisterScreen /> },
+      {
+        path: "",
+        element: <PrivateRoute />,
+        children: [{ path: "profile", element: <ProfileScreen /> }],
+      },
     ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
 );
